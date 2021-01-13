37487e3c4ca30bdc59a649168570969828daa8c4
Martín Campos <martin.stepwolf@gmail.com>
2021-01-13 15:39:26 -0600

    3 day: regex-validations improved

--- 1-regex-validations.py
+++ 1-regex-validations.py
@@ -3,88 +3,108 @@ import re
 def validation(text):
     rules = [
         {
-            'pattern': r"([0-1]+)",
-            'message': 'It is a binary code'
+            'pattern': "[0-1]+",
+            'concept': 'Binary code'
             # example match:  0101
         },
         {
-            'pattern': r"([0-9A-Fa-f]+)",
-            'message': 'It is a hexadecimal code'
+            'pattern': "[0-9A-Fa-f]+",
+            'concept': 'Hexadecimal code'
             # example match:  FF101145A5 or ffaa56
         },
         {
-            'pattern': r"(([0-9a-fA-F]{3}){1,2})",
-            'message': 'It is a hexadecimal code of a color'
+            'pattern': "([0-9a-fA-F]{3}){1,2}",
+            'concept': 'Hexadecimal code of a color'
             # example match:  fff, 123fff
         },
         {
-            'pattern': r"([/d+])", # \d or [0-9]
-            'message': 'It is digit numbers'
+            'pattern': "[/d+]", # \d or [0-9]
+            'concept': 'Numbers'
         },
         {
-            'pattern': r"(.+?[,\n]{1,1}){2,}",
+            'pattern': "(.+?[,\n]{1,1}){2,}",
             # Note: ".+?" is more concrete than just ".+"
-            'message': 'It looks like a csv content'
+            'concept': 'CSVcontent'
             # example match:  123,asdas,123
         },
-        # {
-        #     'pattern': r"([^\d]*[/w]+[^\d]*)",
-        #     'message': 'It is words'
-        # },
-        # {
-        #     'pattern': r"(/W+)",
-        #     'message': 'It is not words'
-        # },
-        # {
-        #     'pattern':  r"([^0-9][a-zA-Z]+[^0-9])",
-        #     'message': 'It is letters'
-        # },
-        # {
-        #     'pattern': r"(\w*\.)",
-        #     'message': 'It has a . at the final'
-        # },
-        # {
-        #     'pattern': r"(\w*\.{1}?)",
-        #     'message': 'It is a paragraph with some .'
-        # },
-        # {
-        #     'pattern': r"([\d*\s+\d*])",
-        #     'message': 'It has spaces'
-        # },
-        # {
-        #     'pattern': r"([\d*\S+\d*])",
-        #     'message': 'It does not have spaces'
-        # },
         {
-            'pattern': r"([^1-9][a-z]{3,}(s|es))",
-            'message': 'It could be a word in plural'
+            'pattern':  "[a-zA-Z]+",
+            'concept': 'Letters'
+            # example match: developer, Python
+        },
+        {
+            'pattern': "/W+",
+            'concept': 'Not words'
+        },
+        {
+            'pattern': "\S+",
+            'concept': 'Content without spaces'
+        },
+        {
+            'pattern': "[^1-9][a-z]{3,}(s|es|ies)",
+            'concept': 'Word in plural'
             # example match:  words, parties
         },
         {
-            'pattern': r"(w{3,3}\.[^\W][\w]{2,}\.com)",
-            'message': 'It is a basic URL'
+            'pattern': "[\w\.]{3,}@[\w\.]{3,}\.\w{2,5}",
+            'concept': 'Email'
+            # example match:  user@mail.com or user.new@mail.com
+        },
+        {
+            'pattern': "w{3,3}\.[^\W][\w]{2,}\.com",
+            'concept': 'Basic URL'
             # example match:  www.url.com
         },
         {
-            'pattern': r"((\d{2,2}[\-\.\ ]?){3,3})",
-            'message': 'It is a phone number'
+            'pattern': "https?://www\.[^\W][\w]{2,}\.com(\.\w{1,})?",
+            'concept': 'Complete URL'
+            # example match:  https://www.url.com or http://www.url.com.mx
+        },
+        {
+            'pattern': "(\d{2,2}[\W]?){3,3}",
+            'concept': 'Phone number'
             # example match:  99 99 99, 99-99-99 or 99.99.99
         },
         {
-            'pattern': r"(\+\d{1,3}\s?(\d{3}\s?){2}\d{2}\s?\d{2})",
-            'message': 'It is a international phone number'
+            'pattern': "\+\d{1,3}\s?(\d{3}\s?){2}\d{2}\s?\d{2}",
+            'concept': 'International phone number'
             # example match: +123 123 123 12 12 or +11231231212
         },
-
+        {
+            'pattern': "(\d{1,3}\.){3}\d{1,3}",
+            'concept': 'IPv4 address or Wildcard mask'
+            # example match: 0.0.0.255 or 999.999.999.999
+            # Note: 999.999.999.99 cannot be a IPv4
+            # The programming language should to validate the value
+            # Regex can only validate the format in this case
+        },
+        {
+            'pattern': "\-?\d{1,3}\.\d{1,6},\s?\-?\d{1,3}\.\d{1,6}",
+            'concept': 'Geographical coordinates'
+            # example match: '-78.123223,100.12' or '999.123, 390.123'
+            # Note: 999.123, 390.123 cannot be a coordinates
+            # The programming language should to validate the value
+            # Regex can only validate the format in this case
+        },
+        {
+            'pattern': "([A-ZÑÁÉÍÓÚ][a-zñáéíóú]+\s?){1,5}",
+            'concept': 'Name of a person from Mexico'
+            # example match: Edgar Julio Ponce Rodríguez, Martín Campos 
+        }
     ]
-    print('===== Validations =====')
+    print("===== Validations =====")
+    print("Your input could be:")
     for rule in rules:
-        if re.match(rule['pattern'], text):
-            print("- " + rule['message'])
+        if re.match(r"(^" + rule['pattern'] +"$)", text):
+            # ^ (at the beginning) and $ (at the final) make the match more exactly
+            # example: Martin is a name, but www.Martin.mx is not a name even if it contains it
+            print("- " + rule['concept'])
 
 def run():
     print("1-regex (regular expressions).")
-    text = input("Type whatever you want: ")
+    text = ""
+    while text == "":
+        text = input("Type whatever you want, (not null input): ")
     validation(text)
 
 if __name__ == "__main__":
--- log.md
+++ log.md
@@ -10,6 +10,14 @@
 
 **Today's Progress**: Learn the basic about regular expression (regex) and create a programm in python with validations to define what the user typed.
 
-**Thoughts:** An powerful programming tool for search and/or validate data as text, email, url, name, phone number or any information with an specific pattern. Many pattern does not work as I want, so they were commented at the moment.
+**Thoughts:** An powerful programming tool to search and/or validate data as text, email, url, name, phone number or any information with an specific pattern. Many pattern does not work as I want, so they were commented at the moment.
+
+**File:** [1-regex-validations.py](1-regex-validations.py)
+
+### Day 3: January 13th, 2021
+
+**Today's Progress**: Learn more about regular expression (regex) and, create more expressions and optimize the initial programm.
+
+**Thoughts:** Even the regex are powerful, this is only a tool, more validations is done by the programming language (examples about Ipv4 and geographical coordinates in the programm).
 
 **File:** [1-regex-validations.py](1-regex-validations.py)
